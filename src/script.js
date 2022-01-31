104c45a89f01de3e6fc596eee8351c3bfb2c839f
alexandravelascosvoboda <alex.v.s@live.com>
2022-01-31 13:55:47 +0100

    Image fix

--- src/script.js
+++ src/script.js
@@ -61,29 +61,34 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `   
-              <div class="col-2">
-              <div class="weather-forecast-wrapper">
-                <div class="weather-forecast-date">${formatDay(
-                  forecastDay.dt
-                )}</div>
-                
-                <img 
-                  src="http://openweathermap.org/img/wn/${
-                    forecastDay.weather[0].icon
-                  }@2x.png"
-                  alt=""
-                  width="36"
-                />
-                <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> ${Math.round(
-                    forecastDay.temp.max
-                  )}° </span>
-                  <span class="weather-forecast-temperature-min">${Math.round(
-                    forecastDay.temp.min
-                  )}° </span>
-                </div>
+            <div class="col-sm-2"> 
+              <div class="weather-forecast-wrapper container-fluid">
+                <div class="row">
+                    <div class="weather-forecast-date">${formatDay(
+                      forecastDay.dt
+                    )}</div>
+                      <div>
+                      <img 
+                        src="http://openweathermap.org/img/wn/${
+                          forecastDay.weather[0].icon
+                        }@2x.png"
+                        alt=""
+                        width="36" />
+                      </div>
+                        
+                      
+                      <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max"> ${Math.round(
+                          forecastDay.temp.max
+                        )}° </span>
+                        <span class="weather-forecast-temperature-min">${Math.round(
+                          forecastDay.temp.min
+                        )}° </span>
+                      </div>
+                          
+                </div>  
               </div>
-              </div>
+            </div> 
             `;
     }
   });
